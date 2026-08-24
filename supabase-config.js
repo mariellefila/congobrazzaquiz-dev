5bb4de660381109749615adec0147e0d1f7c4abe
mariellefila <mariellefila_fr@yahoo.fr>
2026-08-24 19:07:09 +0000

    Publish Supabase frontend config

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+// Auto-generated file for local runtime configuration.
+// Do not commit this file.
+window.SUPABASE_URL = "https://xbsequfhjqdcarsidxxa.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_6mge4xp_U0Som6J68Ep7AQ_veoelzz7";
